a6873c1988088cc73a52196d0d710251f802b022
dektlong <dekel.tankel@gmail.com>
2019-01-23 20:12:35 -0800

    Update Hello.java

--- src/main/java/functions/Hello.java
+++ src/main/java/functions/Hello.java
@@ -5,7 +5,7 @@ import java.util.function.Function;
 public class Hello implements Function<String, String> {
 
 	public String apply(String name) {
-		String retval = "Hello " + name;
+		String retval = "I am saying " + name;
 		System.out.println(retval);
 		return retval;
 	}
